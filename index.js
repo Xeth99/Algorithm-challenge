d4095f9f9d3ef80096ae384e05ecc455beb4f557
CXZ3th <124794318+Xeth99@users.noreply.github.com>
2023-12-26 19:31:16 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
 // create a function that multiplies each element in an array by two and returns the first element and the transformed array in another array without mutating the original array
 
 function arr1(num){
-    let newArr = [num[0]*2];
+    let newArr = [];
     for(let i = 0; i < num.length; i++){
         newArr.push(num[i] * 2);
     }
     
-    return newArr;
+    return [num[0],newArr];
 }
-console.log(arr1([1,2,3,4,5]));
